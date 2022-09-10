16296568e85d0e5d26033963d335379b3ab1b52d
Vic L <110203501+vlofton@users.noreply.github.com>
2022-09-10 16:01:58 -0400

    Update test.js

---
+++ test.js
@@ -0,0 +1,13 @@
+const { describe } = require("mocha");
+
+var expect = chai.expect;
+
+describe(`${checkingscores}`, chekingscores() {
+    describe('ifequal', ifqual() {
+        it('should check scores of each player', chekingscores() {
+            var x = ifqual('Tie');
+            expect(x).to.equal('its a tie');
+        });
+        
+    });
+});
